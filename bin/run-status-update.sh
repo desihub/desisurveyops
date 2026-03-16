56387f064e18fb02a7f8d67ead9dad036010f8fb
Yufeng Luo <brookluo34@hotmail.com>
2026-03-16 13:06:42 -0700

    Update the cronjob settings

--- bin/run-status-update.sh
+++ bin/run-status-update.sh
@@ -2,4 +2,4 @@
 
 source /global/cfs/cdirs/desi/software/desi_environment.sh main
 export PYTHONPATH=/global/homes/b/brookluo/desihub/desisurveyops/py:$PYTHONPATH
-/global/homes/b/brookluo/desihub/desisurveyops/bin/desi_tertiary_status --prognum $(cat /global/u1/b/brookluo/desihub/mydesi/desisurveyops/bin/tertiary-progress.txt)  --outdir /global/cfs/cdirs/desicollab/users/brookluo/tertiary-status --numproc 2
+/global/homes/b/brookluo/desihub/desisurveyops/bin/desi_tertiary_status --prognum $(cat /global/u1/b/brookluo/desihub/mydesi/desisurveyops/bin/tertiary-progress.txt)  --outdir /global/cfs/cdirs/desicollab/users/brookluo/tertiary-status --numproc 4
